a1b602660835d7fd32ac85d74bad59bd26165af7
«@Edu026» <edu.amoros26@gmail.com>
2024-01-30 18:19:53 +0100

    DONE

--- app/src/main/java/com/example/sensors_moviment/MainActivity.java
+++ app/src/main/java/com/example/sensors_moviment/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.sensors_moviment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    SensorEventListener sensorListener;
+
+    private TextView txtX;
+    private TextView txtY;
+    private TextView txtZ;
+
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtX = findViewById(R.id.resX);
+        txtY = findViewById(R.id.resY);
+        txtZ = findViewById(R.id.resZ);
+
+
+        sensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // Valors de l'acceleròmetre en m/s^2
+                float xAcc = sensorEvent.values[0];
+                float yAcc = sensorEvent.values[1];
+                float zAcc = sensorEvent.values[2];
+
+                txtX.setText(String.valueOf(xAcc));
+                txtY.setText(String.valueOf(yAcc));
+                txtZ.setText(String.valueOf(zAcc));
+
+
+
+                // Processament o visualització de dades...
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+                // Es pot ignorar aquesta CB de moment
+            }
+        };
+
+        // Seleccionem el tipus de sensor (veure doc oficial)
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // registrem el Listener per capturar els events del sensor
+        if( sensor!=null ) {
+            sensorManager.registerListener(sensorListener,sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 }
+
